7bcd13137d9e982856151098a8f3699d76465bc5
Jinyan Liu <jinyan.liu@truecaller.com>
2019-12-04 20:21:01 +0100

    Day 4

---
+++ days/day_4/constant.py
@@ -0,0 +1,10 @@
+"""
+Created at 2019-12-04 19:33
+
+@author: jinyanliu
+"""
+
+START_NUMBER = 130254
+END_NUMBER = 678275
+
+DIGITS_LIMIT = 6
--- days/day_4/day_4.py
+++ days/day_4/day_4.py
@@ -3,35 +3,53 @@ Created at 2019-12-04 10:23
 
 @author: jinyanliu
 """
-
-start_number = 130254
-end_number = 678275
+from days.day_4.constant import START_NUMBER, END_NUMBER, DIGITS_LIMIT
 
 
 def get_answer_to_question_1():
     valid_passwords = []
-    for number in range(start_number, end_number):
-        if is_sorted(number) and has_same_adjacent_digits(number):
+    for number in range(START_NUMBER, END_NUMBER):
+        if is_sorted(number) and has_multiple_same_adjacent_digits(number):
+            valid_passwords.append(number)
+    return len(valid_passwords)
+
+
+def get_answer_to_question_2():
+    valid_passwords = []
+    for number in range(START_NUMBER, END_NUMBER):
+        if is_sorted(number) and has_2_same_adjacent_digits(number):
             valid_passwords.append(number)
     return len(valid_passwords)
 
 
 def is_sorted(number):
     sorted_number = int("".join(sorted(str(number))))
-    if sorted_number == number:
-        return True
-    else:
-        return False
+    return True if sorted_number == number else False
 
 
-def has_same_adjacent_digits(number):
+def has_multiple_same_adjacent_digits(number):
     number_str = str(number)
-    for i in range(0, 5):
+    for i in range(0, DIGITS_LIMIT - 1):
         if number_str[i] == number_str[i + 1]:
+            return True
+    return False
+
+
+def has_2_same_adjacent_digits(number):
+    number_str = str(number)
+
+    if number_str[0] == number_str[1] and number_str[1] != number_str[2]:
+        return True
+    if number_str[DIGITS_LIMIT - 2] == number_str[DIGITS_LIMIT - 1] \
+            and number_str[DIGITS_LIMIT - 2] != number_str[DIGITS_LIMIT - 3]:
+        return True
+    for i in range(1, DIGITS_LIMIT - 1):
+        if number_str[i] == number_str[i + 1] and number_str[i] != number_str[i - 1] and number_str[i + 1] != \
+                number_str[i + 2]:
             return True
     return False
 
 
 if __name__ == "__main__":
     print("Answer to question 1 = ", get_answer_to_question_1())
-    print("Answer to question 2 = ")
+    print("Answer to question 2 = ", get_answer_to_question_2())
