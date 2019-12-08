184bc2697a17d29493f58ab27065e9c2ad494de9
Jinyan Liu <jinyan.liu@truecaller.com>
2019-12-08 11:18:04 +0100

    Refactor Day 8 Part 1

--- days/day_8/day_8.py
+++ days/day_8/day_8.py
@@ -16,8 +16,7 @@ def split_list(original_list, total_digits_of_one_layer):
             range(0, len(original_list), total_digits_of_one_layer)]
 
 
-def get_solution_1():
-    string_lists = split_list(get_original_string(), 25 * 6)
+def get_index_of_min_0_counts(string_lists):
     dict_of_counts_0 = {}
     for i in range(0, len(string_lists)):
         dict_of_counts_0[i] = 0
@@ -27,12 +26,16 @@ def get_solution_1():
 
     min_counts_of_0 = dict_of_counts_0[0]
     min_counts_of_0_index = 0
-    j = 0
     for j in dict_of_counts_0.keys():
         if dict_of_counts_0[j] < min_counts_of_0:
             min_counts_of_0 = dict_of_counts_0[j]
             min_counts_of_0_index = j
+    return min_counts_of_0_index
 
+
+def get_solution_1():
+    string_lists = split_list(get_original_string(), 25 * 6)
+    min_counts_of_0_index = get_index_of_min_0_counts(string_lists)
     target_string = string_lists[min_counts_of_0_index]
     count_of_1 = 0
     count_of_2 = 0
@@ -41,7 +44,7 @@ def get_solution_1():
             count_of_1 += 1
         elif a == '2':
             count_of_2 += 1
-    return count_of_2 * count_of_1
+    return count_of_1 * count_of_2
 
 
 if __name__ == "__main__":
